81a58db0119a9078cca9dad7a1ea34116834f09e
tuunit <jan@larwig.com>
2024-08-22 11:46:36 +0200

    templ highlighting support

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,4 +1,4 @@
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "javascript", "typescript", "go", "c", "lua", "vim", "vimdoc", "query" },
 
@@ -15,3 +15,9 @@ require'nvim-treesitter.configs'.setup {
     additional_vim_regex_highlighting = false,
   },
 }
+
+vim.filetype.add({
+  extension = {
+    templ = "templ",
+  },
+})
