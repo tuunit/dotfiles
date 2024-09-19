ac37ce4b9c393017d5879e3b1ca0ec67c79d82ca
tuunit <jan@larwig.com>
2024-09-19 21:39:21 +0200

    support tun and nordvpn

--- i3/.config/polybar/vpn-status.sh
+++ i3/.config/polybar/vpn-status.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 # Count the number of tun adapters, e.g. tun0, tun1, etc
 TUNTAPS=$(ip -j tuntap list | jq -r '.[].ifname' | grep -c '^tun' )
+NORD=$(ip -j a | jq -r '.[].ifname' | grep -c 'nordlynx')
 
-if [ "$TUNTAPS" != "0" ]; then
+if [[ "$TUNTAPS" != "0" || "$NORD" != "0" ]]; then
     # This filters the JSON output from ip to show "name ip.ip.ip.ip" for each adapter
     MESSAGE_UP=$(\
         ip -4 -j addr show \
-        | jq '[ .[] | select( .ifname | startswith("tun")) | .addr_info[0].local ]'\
+        | ip -4 -j addr show | jq '[ .[] | select( .ifname | (startswith("nord") or startswith("tun")) ) | .addr_info[0].local ]' \
         | jq -r 'join(", ")'\
     )
     echo "🛡️ %{u#55aa55}${MESSAGE_UP}%{u-}"
