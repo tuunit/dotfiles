09e6177a2bd12490fab6d16dfb2dd434e3963c97
tuunit <jan@larwig.com>
2024-09-24 14:02:19 +0200

    random wallpaper

--- i3/.config/i3/random_wallpaper.sh
+++ i3/.config/i3/random_wallpaper.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-while true; do
-  curl -sL https://unsplash.it/3440/1440/\?random | feh --bg-fill -
-  sleep 300
-done
+if [[ $# -eq 0 ]]; then
+  while true; do
+    curl -sL https://unsplash.it/3440/1440/\?random | feh --bg-fill -
+    sleep 300
+  done
+else
+    curl -sL https://unsplash.it/3440/1440/\?random | feh --bg-fill -
+fi
