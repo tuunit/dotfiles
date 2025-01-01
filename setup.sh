fa25ef778c86e9a98decf20b1e0ea7d6fd2f2019
Jan Larwig <jan@larwig.com>
2025-01-01 16:50:48 +0000

    add goenv install

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ done
 
 sudo apt install -yq kitty thunar zsh brightnessctl rofi polybar ksnip ripgrep btop fzf xdotool xclip
 
+git clone https://github.com/go-nv/goenv.git ~/.goenv
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 chsh -s $(which zsh)
 
