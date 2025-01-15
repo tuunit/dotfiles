1d2e70989cbcf3ae7fc75a02f040b9b40e782ea6
Jan Larwig <jan@larwig.com>
2025-01-15 09:25:09 +0100

    polybar tweaks and xrandr config

--- i3/.config/i3/configure_xrandr.sh
+++ i3/.config/i3/configure_xrandr.sh
@@ -3,7 +3,7 @@
 connected=$(xrandr | grep " connected " | awk '{print $1}')
 
 if [[ $connected =~ "eDP-1" && $connected =~ "HDMI-1" && $connected =~ "DP-3" ]]; then
-  xrandr --output eDP-1 --mode 1920x1200 --pos 6000x515 --rotate normal --output HDMI-1 --primary --mode 3440x1440 --pos 2560x0 --rotate normal --output DP-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off
+  xrandr --output eDP-1 --mode 1920x1200 --pos 6000x515 --rotate normal --output HDMI-1 --primary --mode 3440x1440 --pos 2560x0 --rotate normal --output DP-3 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off --output DP-1 --off --output DP-4 --off
 elif [[ $connected == "eDP-1" ]]; then
   xrandr --output eDP-1 --mode 1920x1200 --pos 0x0 &
 elif [[ $connected =~ "DP-4" && $connected =~ "DP-2" ]]; then
