f2e27e63e8b05b5823839866c01323892b66d96c
Jan Larwig <jan@larwig.com>
2025-07-15 17:34:17 +0200

    reconfigure monitor layout
    Signed-off-by: Jan Larwig <jan@larwig.com>

--- i3/.config/i3/configure_xrandr.sh
+++ i3/.config/i3/configure_xrandr.sh
@@ -3,9 +3,9 @@
 connected=$(xrandr | grep " connected " | awk '{print $1}')
 
 if [[ $connected =~ "eDP-1" && $connected =~ "HDMI-1" && $connected =~ "DP-3" ]]; then
-  xrandr --output eDP-1 --mode 1920x1200 --pos 6000x515 --rotate normal --output HDMI-1 --primary --mode 3440x1440 --pos 2560x0 --rotate normal --output DP-3 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off --output DP-1 --off --output DP-4 --off
+  xrandr --output eDP-1 --primary --mode 1920x1200 --pos 0x740 --rotate normal --output HDMI-1 --mode 3440x1440 --pos 1920x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --mode 2560x1440 --pos 5360x0 --rotate normal --output DP-4 --off
 elif [[ $connected == "eDP-1" ]]; then
   xrandr --output eDP-1 --mode 1920x1200 --pos 0x0 &
-elif [[ $connected =~ "DP-4" && $connected =~ "DP-2" ]]; then
-  xrandr --output DP-4 --mode 2560x1440 --pos 0x0 --output DP-2 --mode 3440x1440 --pos 2560x0 &
+elif [[ $connected =~ "DP-2" && $connected =~ "DP-4" ]]; then
+  xrandr --output DP-4 --mode 3440x1440 --pos 0x0 --output DP-2 --mode 2560x1440 --pos 3440x0 &
 fi
