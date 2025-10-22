088f9f2cf6b5dfb0bb9bacd93bdaffc2679d80b8
Jan Larwig <jan@larwig.com>
2025-10-22 18:25:05 +0200

    reconfigure screen layout
    Signed-off-by: Jan Larwig <jan@larwig.com>

--- i3/.config/i3/configure_xrandr.sh
+++ i3/.config/i3/configure_xrandr.sh
@@ -7,5 +7,5 @@ if [[ $connected =~ "eDP-1" && $connected =~ "HDMI-1" && $connected =~ "DP-3" ]]
 elif [[ $connected == "eDP-1" ]]; then
   xrandr --output eDP-1 --mode 1920x1200 --pos 0x0 &
 elif [[ $connected =~ "DP-2" && $connected =~ "DP-4" ]]; then
-  xrandr --output DP-4 --mode 3440x1440 --pos 0x0 --output DP-2 --mode 2560x1440 --pos 3440x0 &
+  xrandr --output DP-2 --mode 3440x1440 --pos 0x0 --output DP-4 --mode 2560x1440 --pos 3440x0 &
 fi
