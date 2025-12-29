60c374e05d801e037fa1a2c69dca4748823ff527
Jan Larwig <jan@larwig.com>
2025-12-29 22:08:20 +0100

    add debugging
    Signed-off-by: Jan Larwig <jan@larwig.com>

---
+++ nvim-new/.config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,51 @@
+return {
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			"nvim-neotest/nvim-nio",
+			"leoluz/nvim-dap-go",
+		},
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+
+			dapui.setup()
+			require("dap-go").setup()
+
+			-- Auto-open/close UI
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+
+			vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Continue" })
+			vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Debug: Step Over" })
+			vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Debug: Step Into" })
+			vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Debug: Step Out" })
+			vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+			vim.keymap.set("n", "<leader>B", function()
+				dap.set_breakpoint(vim.fn.input("Condition: "))
+			end)
+			vim.keymap.set("n", "<leader>dr", dapui.toggle, { desc = "Debug: Toggle UI" })
+		end,
+	},
+	{
+		"theHamsta/nvim-dap-virtual-text",
+		config = function()
+			require("nvim-dap-virtual-text").setup({
+				enabled = true,
+				all_frames = false,
+				commented = false,
+			})
+		end,
+	},
+}
