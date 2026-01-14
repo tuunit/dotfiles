80fdf0963ac1742ba3ade7a2c121e05ebea0bc7f
Jan Larwig <jan@larwig.com>
2026-01-14 08:27:36 +0100

    force reload i3 and xrandr configuration
    Signed-off-by: Jan Larwig <jan@larwig.com>

--- i3/.config/i3/configure_xrandr.sh
+++ i3/.config/i3/configure_xrandr.sh
@@ -3,6 +3,7 @@
 connected=$(xrandr | grep " connected " | awk '{print $1}')
 
 if [[ $connected =~ "eDP-1" && $connected =~ "HDMI-1" && $connected =~ "DP-3" ]]; then
+  xrandr --output eDP-1 --off --output DP-3 --off --output HDMI-1 --off && \
   xrandr --output eDP-1 --primary --mode 1920x1200 --pos 0x700 --output DP-3 --mode 3440x1440 --pos 1920x0 --output HDMI-1 --mode 2560x1440 --pos 5360x0 &
 elif [[ $connected == "eDP-1" ]]; then
   xrandr --output eDP-1 --mode 1920x1200 --pos 0x0 &
