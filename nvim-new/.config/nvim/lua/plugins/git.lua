250c86d36ccc421d15a72d4a64233fe63cd8689d
Jan Larwig <jan@larwig.com>
2026-02-07 20:56:52 +0100

    feat: better git integration
    Signed-off-by: Jan Larwig <jan@larwig.com>

--- nvim-new/.config/nvim/lua/plugins/git.lua
+++ nvim-new/.config/nvim/lua/plugins/git.lua
@@ -1,3 +1,24 @@
 return {
-  { 'tpope/vim-fugitive' },
+	-- Git integration
+	{ "tpope/vim-fugitive" },
+	-- Git diff viewer
+	{
+		"sindrets/diffview.nvim",
+		init = function()
+			vim.opt.fillchars = {
+				diff = "╱",
+			}
+			vim.opt.diffopt = {
+				"internal",
+				"filler",
+				"closeoff",
+				"context:12",
+				"algorithm:histogram",
+				"linematch:200",
+				"indent-heuristic",
+			}
+		end,
+	},
+	-- Github integration
+	{ "tpope/vim-rhubarb" },
 }
