ea6b35cb34bc1751a5ea2928be373099384da36e
Jan Larwig <jan@larwig.com>
2026-09-11 14:53:51 +0200

    fix treesitter config syntax for auto installs
    Signed-off-by: Jan Larwig <jan@larwig.com>

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,10 +4,8 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     lazy = false,
     build = ":TSUpdate",
-    opts = {
-      ensure_installed = { "go", "lua", "bash", "json" },
-      highlight = { enable = true },
-      indent = { enable = true },
-    },
+    config = function()
+      require("nvim-treesitter").install({ "go", "lua", "bash", "json" })
+    end,
   },
 }
